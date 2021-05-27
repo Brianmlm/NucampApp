d740782af2d7915e3f99e4d6caaa4a9b66486d8a
Brian <Babji324080@gmail.com>
2021-05-26 20:59:56 -0500

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import Main from './components/MainComponent'
-import { Provider } from 'react-redux' 
+import { Provider } from 'react-redux'
 import { ConfigureStore } from './redux/configureStore' //Creates, configures and returns the redux store
+import { PersistGate } from 'redux-persist/es/integration/react'
+import Loading from './components/LoadingComponent'
+
 console.disableYellowBox = true //Disables Yellow error messages
 
-const store = ConfigureStore(); 
+const { persistor, store } = ConfigureStore()
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
-    
   )
-}
-
-
+} //PersistGate prevents app from rendering until component has fully re-hydrated from the redux store
